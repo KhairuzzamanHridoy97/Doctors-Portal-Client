358b5d2bba4a9be5867010e1eaed50b4bcac82c8
KhairuzzamanHridoy97 <khairuzzaman15-1273@diu.edu.bd>
2022-02-02 19:19:44 +0600

    styling banner section

---
+++ src/Pages/Home/Banner/Banner.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import chair from '../../../images/chair.png';
+import bg from '../../../images/bg.png';
+
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+import { Button, Container, Typography } from '@mui/material';
+
+const bannerBg={
+    background:`url(${bg})`,
+}
+
+const verticalCenter={
+    display:'flex',
+    height:400,
+    alignItems:'center',
+    border:'1px solid red'
+} 
+
+const Banner = () => {
+    return (
+        <Container style={bannerBg} sx={{ flexGrow: 1 }}>
+      <Grid container spacing={2}>
+        <Grid  item style={{textAlign:'left', ...verticalCenter}} xs={12} md={5}>
+            <Box>
+            <Typography  variant='h3'>
+                Your New Smile <br /> Start's Here
+          </Typography>
+          <Typography variant='h6' sx={{fontSize:14,color:'gray',fontWeight:300}}>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Amet magnam repudiandae voluptate rerum delectus? Ea laborum fugiat nesciunt expedita laboriosam.
+          </Typography>
+          <Button variant='contained'>
+              Get Appoinment
+          </Button>
+            </Box>
+        </Grid>
+        <Grid item  xs={12} md={7} style={verticalCenter}>
+            <img style={{width:'350px'}} src={chair} alt="" />
+        </Grid>
+       
+      </Grid>
+    </Container>
+    );
+};
+
+export default Banner;
