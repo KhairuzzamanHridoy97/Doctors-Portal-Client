7f596dcba8316e4a37953d20b66221c818392a7d
KhairuzzamanHridoy97 <khairuzzaman15-1273@diu.edu.bd>
2022-02-02 16:35:52 +0600

    added appoinment banner

---
+++ src/Pages/Home/AppoinmentBanner/AppoinmentBanner.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AppoinmentBanner = () => {
+    return (
+        <div>
+            <h2>This is AppoinmetBanner</h2>
+        </div>
+    );
+};
+
+export default AppoinmentBanner;
