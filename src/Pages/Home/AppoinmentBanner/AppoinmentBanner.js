7d45e77f6c1eea127b6b838ee3a3b423b985d1f2
KhairuzzamanHridoy97 <khairuzzaman15-1273@diu.edu.bd>
2022-02-02 17:00:34 +0600

    added appoinment bg

--- src/Pages/Home/AppoinmentBanner/AppoinmentBanner.js
+++ src/Pages/Home/AppoinmentBanner/AppoinmentBanner.js
@@ -1,10 +1,31 @@
 import React from 'react';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+import doctor from '../../../images/doctor.png'
+import bg from '../../../images/appointment-bg.png'
+import { Typography } from '@mui/material';
+
+const appoinmentBg={
+    background:`url(${bg})`
+}
 
 const AppoinmentBanner = () => {
     return (
-        <div>
-            <h2>This is AppoinmetBanner</h2>
-        </div>
+        <Box style={appoinmentBg} sx={{ flexGrow: 1 }}>
+            <Grid container spacing={2}>
+
+                <Grid item xs={12} md={6}>
+                <img style={{width:"400px"}}  src={doctor} alt="" />
+                </Grid>
+                <Grid item xs={6} md={4}>
+                <Typography variant="h6">
+                        Appoinment
+                </Typography>
+                </Grid>
+                
+            </Grid>
+        </Box>
     );
 };
 
