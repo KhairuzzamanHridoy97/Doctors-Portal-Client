83e54322cae96fbb8a4b2d6e3c2eaa4724247a3e
KhairuzzamanHridoy97 <khairuzzaman15-1273@diu.edu.bd>
2022-02-07 18:44:12 +0600

    style booking

--- src/Pages/Appointment/Booking/Booking.js
+++ src/Pages/Appointment/Booking/Booking.js
@@ -1,11 +1,25 @@
-import { Grid } from '@mui/material';
+import { Button, Grid, Paper } from '@mui/material';
 import React from 'react';
+import Typography from '@mui/material/Typography';
 
 const Booking = ({booking}) => {
     const {name,time,space}= booking;
     return (       
             <Grid item xs={12} sm={6} md={4}>                    
-                <h2>This is Booking</h2>
+                <Paper elevation={3} sx={{py:5}}>
+                    <Typography sx={{color:'info.main'}} variant="h5" gutterBottom component="div">
+                        {name}
+                    </Typography>
+                    <Typography variant="h6" gutterBottom component="div">
+                        {time}
+                    </Typography>
+                    <Typography variant="caption" gutterBottom component="div">
+                        {space} SPACES AVAILABLE
+                    </Typography>
+                    <Button variant='contained'>
+                        BOOK APPOINMENT
+                    </Button>
+                </Paper>
             </Grid>   
     );
 };
