3a4bc72b45156ae54aec09cffa4c36ffb3f6105f
KhairuzzamanHridoy97 <khairuzzaman15-1273@diu.edu.bd>
2022-02-07 18:24:38 +0600

    added booking

--- src/Pages/Appointment/AvailableAppoinments/AvailableAppoinments.js
+++ src/Pages/Appointment/AvailableAppoinments/AvailableAppoinments.js
@@ -1,10 +1,62 @@
+import { Container, Grid } from '@mui/material';
 import React from 'react';
+import Booking from '../Booking/Booking'
+
+const bookings =[
+    {
+        id:1,
+        name:"Teeth Orthdonics",
+        time:'11.00 AM - 12.00 PM',
+        space:10
+    },
+    {
+        id:2,
+        name:"Cosmetic Dentistry",
+        time:'11.00 AM - 12.00 PM',
+        space:8
+    },
+    {
+        id:3,
+        name:"Teeth Cleaning",
+        time:'11.00 AM - 12.00 PM',
+        space:3
+    },
+    {
+        id:4,
+        name:"Cavity Protection",
+        time:'08.00 AM - 12.00 PM',
+        space:4
+    },
+    {
+        id:5,
+        name:"Cavity Protection",
+        time:'10.00 AM - 11.00 PM',
+        space:6
+    },
+    {
+        id:6,
+        name:"Oral Sergery",
+        time:'11.00 AM - 12.00 PM',
+        space:7
+    },
+]
+
 
 const AvailableAppoinments = ({date}) => {
-    return (
-        <div>
-            <h2>Available Appoinments {date.toDateString()}</h2>
-        </div>
+    return (        
+            <Container>
+                <h2>Available Appoinments {date.toDateString()}</h2>
+                <Grid container spacing={2}>
+                   {
+                       bookings.map(booking=>
+                            <Booking
+                            key={booking.id}
+                            booking={booking}
+                            ></Booking>
+                        )
+                   }
+                </Grid>
+            </Container>
     );
 };
 
