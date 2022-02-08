2e9ba385a219064abfbf667662a8cea3a66da064
KhairuzzamanHridoy97 <khairuzzaman15-1273@diu.edu.bd>
2022-02-08 21:41:11 +0600

    added login form

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -1,13 +1,36 @@
-import { Container, Grid } from '@mui/material';
+import { Container, Grid, TextField, Typography } from '@mui/material';
 import React from 'react';
 import login from '../../../images/login.png';
 
 const Login = () => {
+
+    const handleLoginSubmit=(e)=>{
+
+        e.preventDefault();
+    }
+
     return (
         <Container>
             <Grid container spacing={2}>
-                <Grid item xs={12} md={6}>
-                  
+                <Grid item sx={{mt:3}} xs={12} md={6}>
+                <Typography variant="body1" gutterBottom>
+                        Login
+                </Typography>
+                <form onSubmit={handleLoginSubmit}>
+                     <TextField
+                     sx={{width:'75%',m:1}}
+                      id="standard-basic" 
+                      label="Your Email" 
+                      variant="standard"
+                       />
+                     <TextField
+                    sx={{width:'75%',m:1}}
+                      id="standard-basic" 
+                      label="Your Password" 
+                      type='password'
+                      variant="standard"
+                       />
+                </form>
                 </Grid>
 
                 <Grid item xs={12} md={6}>
