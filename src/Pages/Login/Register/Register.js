64bae975980dad1f74c8b9afc96eef504fbc93de
KhairuzzamanHridoy97 <khairuzzaman15-1273@diu.edu.bd>
2022-02-08 23:53:36 +0600

    remove some code in registration

--- src/Pages/Login/Register/Register.js
+++ src/Pages/Login/Register/Register.js
@@ -13,7 +13,6 @@ const Register = () => {
         const value= e.target.value;
         const newLoginData={...loginData};
         newLoginData[field]=value;
-        console.log(newLoginData);
         setLoginData(newLoginData);
     }
 
