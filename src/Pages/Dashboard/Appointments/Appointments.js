047a159f2aa74ee7090cad41a5e23dc888cbc51f
KhairuzzamanHridoy97 <khairuzzaman15-1273@diu.edu.bd>
2022-02-11 12:49:36 +0600

    add dashboard table

--- src/Pages/Dashboard/Appointments/Appointments.js
+++ src/Pages/Dashboard/Appointments/Appointments.js
@@ -1,6 +1,14 @@
 import React ,{useState,useEffect}from 'react';
 // import { useState } from 'react';
 import useAuth from '../../../hooks/useAuth';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+
 
 const Appointments = () => {
     const {user}= useAuth();
@@ -15,7 +23,35 @@ const Appointments = () => {
 
     return (
         <div>
-            <h2>Appointments {appointments.length} </h2>
+            <h2>Appointments: on {appointments.length} </h2>
+            <TableContainer component={Paper}>
+      <Table sx={{ }} aria-label="Appointments Table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Name</TableCell>
+            <TableCell align="right">Time</TableCell>
+            <TableCell align="right">Action</TableCell>
+            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
+            <TableCell align="right">Protein&nbsp;(g)</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {appointments.map((row) => (
+            <TableRow
+              key={row._id}
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">
+                {row.patientName}
+              </TableCell>
+              <TableCell align="right">{row.time}</TableCell>
+              <TableCell align="right">{row.fat}</TableCell>
+            
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
         </div>
     );
 };
